0cda9f6153ed36f7c5f0f09643a67fd0b669ad4d
Tobias de Bruijn <github@thedutchmc.nl>
2021-03-18 18:58:40 +0100

    Changed the way I reload for the player who is applying the skin

--- src/main/java/nl/thedutchmc/SkinFixer/changeSkin/SkinChangeOrchestrator.java
+++ src/main/java/nl/thedutchmc/SkinFixer/changeSkin/SkinChangeOrchestrator.java
@@ -1,5 +1,10 @@
 package nl.thedutchmc.SkinFixer.changeSkin;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
@@ -10,11 +15,14 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import com.google.common.hash.Hashing;
+
 import net.md_5.bungee.api.ChatColor;
 import nl.thedutchmc.SkinFixer.SkinFixer;
 import nl.thedutchmc.SkinFixer.SkinObject;
 import nl.thedutchmc.SkinFixer.changeSkin.changeGameProfile.*;
 import nl.thedutchmc.SkinFixer.fileHandlers.StorageHandler;
+import nl.thedutchmc.SkinFixer.util.ReflectionUtil;
 
 public class SkinChangeOrchestrator {
 	
@@ -86,6 +94,7 @@ public class SkinChangeOrchestrator {
 			@Override
 			public void run() {
 				//NMS is version dependant. So we need to set the correct class to use.
+				//TODO switch this over to use reflection
 				switch(SkinFixer.NMS_VERSION) {
 				case "v1_16_R1": ChangeGameProfile_1_16_r1.changeProfile(player.getUniqueId(), skinValue, skinSignature); break;
 				case "v1_16_R2": ChangeGameProfile_1_16_r2.changeProfile(player.getUniqueId(), skinValue, skinSignature); break;
@@ -106,31 +115,202 @@ public class SkinChangeOrchestrator {
 		player.sendMessage(ChatColor.GOLD + "Done.");
 	}
 	
+	private static void applySkin(Player player, String skinValue, String skinSignature) {
+	}
+	
+	/**
+	 * Reload the Player so the Player's new skin shows for other players and the Player itself
+	 * @param player The Player to reload
+	 */
 	private static void reloadPlayer(Player player) {
 		new BukkitRunnable() {
 			@Override
 			public void run() {
-				Location loc = player.getLocation().clone();
+				Location playerLocation = player.getLocation().clone();
 				
 			    //Reload the player for all online players
-			    for(Player p : Bukkit.getOnlinePlayers()) {
+				//This is so all other Players can see the new skin
+			    Bukkit.getOnlinePlayers().forEach(p -> {
 			    	p.hidePlayer(SkinFixer.INSTANCE, player);
 			    	p.showPlayer(SkinFixer.INSTANCE, player);
-			    }
+			    });
 				
-		        World teleportToWorld = null;
-		        for(World w : Bukkit.getWorlds()) {
-		        	if(!w.equals(loc.getWorld())) teleportToWorld = w;
-		        }
-		                
-		        player.teleport(new Location(teleportToWorld, 0, 255, 0));
-		        new BukkitRunnable() {
-		            @Override
-		            public void run() {
-		            	player.teleport(loc);                       
-		            	player.updateInventory();
+			    //Remove and re-add the Player to the world
+			    //This is so the Player who applied the skin can see their new skin
+			    try {
+			    	//Get the CraftPlayer class and turn our regular Player into an EntityPlayer
+				    Class<?> craftPlayerClass = ReflectionUtil.getBukkitClass("entity.CraftPlayer");
+				    Object entityPlayer = ReflectionUtil.invokeMethod(craftPlayerClass, player, "getHandle");
+
+				    //Get the CraftWorld class and turn our regular World into a WorldServer
+				    Class<?> craftWorldClass = ReflectionUtil.getBukkitClass("CraftWorld");
+				    Object worldServer = ReflectionUtil.invokeMethod(craftWorldClass, playerLocation.getWorld(), "getHandle");
+				    				    				    				    			
+				    //Get the PlayerInteractManagar,
+				    //From that get the Gamemode of the player as an EnumGamemode and the numerical ID for that
+				    Object playerIntManager = ReflectionUtil.getObject(entityPlayer, "playerInteractManager");
+				    Enum<?> enumGamemode = (Enum<?>) ReflectionUtil.invokeMethod(playerIntManager, "getGameMode");
+				    int gamemodeId = (int) ReflectionUtil.invokeMethod(enumGamemode, "getId");
+				    
+				    //Get the World's seed, and hash it with sha256
+				    Object seed = ReflectionUtil.invokeMethod(playerLocation.getWorld(), "getSeed");
+				    long seedHashed = Hashing.sha256().hashString(seed.toString(), StandardCharsets.UTF_8).asLong();
+				    
+				    //Get the EnumGamemode value from the gamemode ID.
+				    //We can't use ReflectionUtil to invoke the method because that convert
+				    //the primitive int to it's wrapper Integer.
+			    	Method getGamemodeByIdMethod = ReflectionUtil.getMethod(enumGamemode.getClass(), "getById", int.class);
+			    	Object gamemodeEnumConst = getGamemodeByIdMethod.invoke(null, gamemodeId);
+				    
+			    	//PacketPlayOutRespawn Class
+				    Class<?> playPacketOutRespawnClass = ReflectionUtil.getNmsClass("PacketPlayOutRespawn");
+
+				    //Instantiate the PacketPlayOutRespawn packet,
+				    //Different Minecraft versions have slightly different constructors, so we have multiple
+				    Object packetPlayOutRespawn;
+				    try {
+				    	// 1.16.1
+				    	
+				    	//TypeKey and DimensionKey
+				    	Object typeKey = ReflectionUtil.invokeMethod(worldServer.getClass().getSuperclass(), worldServer, "getTypeKey");
+				    	Object dimensionKey = ReflectionUtil.invokeMethod(worldServer.getClass().getSuperclass(), worldServer, "getDimensionKey");
+				    	
+				    	//Instantiate the Packet
+				    	packetPlayOutRespawn = ReflectionUtil.invokeConstructor(playPacketOutRespawnClass,
+			    			new Class<?>[] {
+			    				typeKey.getClass(),
+			    				dimensionKey.getClass(),
+			    				long.class,
+			    				enumGamemode.getClass(),
+			    				enumGamemode.getClass(),
+			    				boolean.class,
+			    				boolean.class,
+			    				boolean.class
+			    			}, new Object[] {
+				    			typeKey,
+				    			dimensionKey,
+				    			seedHashed,
+				    			gamemodeEnumConst,
+				    			gamemodeEnumConst,
+				    			
+				    			//isDebugWorld is in the nms class World, which WorldServer extends
+				    			ReflectionUtil.invokeMethod(worldServer.getClass().getSuperclass(), worldServer, "isDebugWorld"),
+				    			
+				    			//isFlatWorld is in the nms class WorldServer for some reason (I expected it in the nms class World)
+				    			ReflectionUtil.invokeMethod(worldServer, "isFlatWorld"),
+				    			true
+			    			});
+				    	
+				    } catch(Exception ignored) {
+				    	// 1.16.2+
+
+				    	// DimensionManager and DimensionKey
+                        Object dimensionManager = ReflectionUtil.invokeMethod(worldServer.getClass().getSuperclass(), worldServer, "getDimensionManager");
+                        Object dimensionKey = ReflectionUtil.invokeMethod(worldServer.getClass().getSuperclass(), worldServer, "getDimensionKey");
+                        
+    				    /* PacketPlayOutRespawn: 
+    				     * Mojang's variable names to their 'I know what this is'-name 
+    				     * a: (DimensionManager) DimensionManager
+    				     * b: (ResourceKey) ResourceKey<World>
+    				     * c: (long) Sha256 of the seed
+    				     * d: (GameType) PlayerGameType
+    				     * e: (GameType) previousPlayerGameType
+    				     * f: (boolean) isDebug
+    				     * g: (boolean) isFlat
+    				     * h: (boolean) keepAllPlayerData 
+    				     * */
+				    	packetPlayOutRespawn = ReflectionUtil.invokeConstructor(playPacketOutRespawnClass,
+			    			new Class<?>[] {
+			    				dimensionManager.getClass(), 
+			    				dimensionKey.getClass(), 
+			    				long.class, 
+			    				enumGamemode.getClass(), 
+			    				enumGamemode.getClass(), 
+			    				boolean.class, 
+			    				boolean.class, 
+			    				boolean.class
+			    			}, new Object[] {
+				    			dimensionManager,
+				    			dimensionKey,
+				    			seedHashed,
+				    			gamemodeEnumConst,
+				    			gamemodeEnumConst,
+				    			
+				    			//isDebugWorld is in the nms class World, which WorldServer extends
+				    			ReflectionUtil.invokeMethod(worldServer.getClass().getSuperclass(), worldServer, "isDebugWorld"),
+				    			
+				    			//isFlatWorld is in the nms class WorldServer for some reason (I expected it in the nms class World)
+				    			ReflectionUtil.invokeMethod(worldServer, "isFlatWorld"),
+				    			true
+				    		});
+			    	}
+				    
+				    //PacketPlayOutPosition
+				    Class<?> packetPlayOutPositionClass = ReflectionUtil.getNmsClass("PacketPlayOutPosition");
+				    Object packetPlayOutPosition = ReflectionUtil.invokeConstructor(packetPlayOutPositionClass, 
+				    		new Class<?>[] { double.class, double.class, double.class, float.class, float.class, Set.class, int.class },
+				    		new Object[] { playerLocation.getX(), playerLocation.getY(), playerLocation.getZ(), playerLocation.getYaw(), playerLocation.getPitch(), new HashSet<Enum<?>>(), 0 });
+				    
+				    //PacketPlayOutHeldItem
+				    Class<?> packetPlayOutHeldItemSlotClass = ReflectionUtil.getNmsClass("PacketPlayOutHeldItemSlot");
+				    Object packetPlayOutHeldItemSlot = ReflectionUtil.invokeConstructor(packetPlayOutHeldItemSlotClass, 
+				    		new Class<?>[] { int.class },
+				    		new Object[] { player.getInventory().getHeldItemSlot() });
+				    
+				    //Get the EntityPlayers' connection
+				    Object playerConnection = ReflectionUtil.getObject(entityPlayer, "playerConnection");
+				    
+				    //Get the Enum constants for REMOVE_PLAYER and ADD_PLAYER
+				    Class<?> packetPlayOutPlayerInfo = ReflectionUtil.getNmsClass("PacketPlayOutPlayerInfo");
+				    Object removePlayerEnumConst = ReflectionUtil.getEnum(packetPlayOutPlayerInfo, "EnumPlayerInfoAction", "REMOVE_PLAYER");
+				    Object addPlayerEnumConst = ReflectionUtil.getEnum(packetPlayOutPlayerInfo, "EnumPlayerInfoAction", "ADD_PLAYER");
+				    				    
+				    //Create an Array of EntityPlayer with size = 1 and add our player to it
+				    Object entityPlayerArr = Array.newInstance(entityPlayer.getClass(), 1);
+				    Array.set(entityPlayerArr, 0, entityPlayer);
+				    
+				    //Construct a PacketPlayOutPlayerInfo with intention REMOVE_PLAYER
+				    Object packetPlayOutRemovePlayer = ReflectionUtil.invokeConstructor(packetPlayOutPlayerInfo, 
+				    		new Class<?>[] { removePlayerEnumConst.getClass(), entityPlayerArr.getClass() }, 
+				    		new Object[] { removePlayerEnumConst, entityPlayerArr });
+				    
+				    //Construct a PacketPlayOutPlayerInfo with intention ADD_PLAYER
+				    Object packetPlayOutAddPlayer = ReflectionUtil.invokeConstructor(packetPlayOutPlayerInfo, 
+				    		new Class<?>[] { addPlayerEnumConst.getClass(), entityPlayerArr.getClass() },
+				    		new Object[] { addPlayerEnumConst, entityPlayerArr });
+				    
+				    //Get the generic Packet class
+				    Class<?> packetClass = ReflectionUtil.getNmsClass("Packet");
+				    
+				    //Send both the PacketPlayOutPlayerInfo packets
+				    ReflectionUtil.invokeMethod(playerConnection, "sendPacket", new Class<?>[] { packetClass }, new Object[] { packetPlayOutRemovePlayer });
+				    ReflectionUtil.invokeMethod(playerConnection, "sendPacket", new Class<?>[] { packetClass }, new Object[] { packetPlayOutAddPlayer });
+				    
+				    //Send the PacketPlayOutRespawn packet
+				    ReflectionUtil.invokeMethod(playerConnection, "sendPacket", new Class<?>[] { packetClass }, new Object[] { packetPlayOutRespawn });
+				    
+				    //Update the Player's abilities
+				    ReflectionUtil.invokeMethod(entityPlayer, "updateAbilities");
+				    
+				    //Send both the PacketPlayOutPosition and PacketPlayOutHeldItem packets
+				    ReflectionUtil.invokeMethod(playerConnection, "sendPacket", new Class<?>[] { packetClass }, new Object[] {packetPlayOutPosition});
+				    ReflectionUtil.invokeMethod(playerConnection, "sendPacket", new Class<?>[] { packetClass }, new Object[] {packetPlayOutHeldItemSlot});
+				    
+				    //Update the Player's healthbar and inventory
+		            ReflectionUtil.invokeMethod(player, "updateScaledHealth");
+		            ReflectionUtil.invokeMethod(player, "updateInventory");
+		            ReflectionUtil.invokeMethod(entityPlayer, "triggerHealthUpdate");
+
+		            //If the Player is OP, we have to toggle it off and back on really quickly for it to work
+		            if(player.isOp()) {
+		            	Bukkit.getScheduler().runTask(SkinFixer.INSTANCE, () -> {
+		            		player.setOp(false);
+		            		player.setOp(true);
+		            	});
 		            }
-		        }.runTaskLater(SkinFixer.INSTANCE, 5L);
+			    } catch(Exception e) {
+			    	e.printStackTrace();
+			    }
 			}
 		}.runTask(SkinFixer.INSTANCE);
 	}
---
+++ src/main/java/nl/thedutchmc/SkinFixer/util/ReflectionUtil.java
@@ -0,0 +1,263 @@
+package nl.thedutchmc.SkinFixer.util;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.bukkit.Bukkit;
+
+public class ReflectionUtil {
+
+	public static String SERVER_VERSION;
+	
+	static {
+		//Load the Bukit class
+		try {
+			Class.forName("org.bukkit.Bukkit");
+		} catch (ClassNotFoundException ignored) {}
+		
+		//Get the version String
+		SERVER_VERSION = Bukkit.getServer().getClass().getPackage().getName().substring(Bukkit.getServer().getClass().getPackage().getName().lastIndexOf('.') + 1);
+	}
+	
+	/**
+	 * Get a Class from the org.bukkit.craftbukkit.SERVER_VERSION. package
+	 * @param className The name of the class
+	 * @return Returns the Class
+	 * @throws ClassNotFoundException Thrown when the Class was not found
+	 */
+	public static Class<?> getBukkitClass(String className) throws ClassNotFoundException {
+		return Class.forName("org.bukkit.craftbukkit." + SERVER_VERSION + "." + className);
+	}
+	
+	/**
+	 * Get a Class from the net.minecraft.server.SERVER_VERSION. package
+	 * @param className The name of the class
+	 * @return Returns the Class
+	 * @throws ClassNotFoundException Thrown when the Class was not found
+	 */
+	public static Class<?> getNmsClass(String className) throws ClassNotFoundException {
+		return Class.forName("net.minecraft.server." + SERVER_VERSION + "." + className);
+	}
+	
+	/**
+	 * Get the Constructor of a Class
+	 * @param clazz The Class in which the Constructor is defined
+	 * @param args Arguments taken by the Constructor
+	 * @return Returns the Constructor
+	 * @throws NoSuchMethodException Thrown when no Constructor in the Class was found with the provided combination of arguments
+	 */
+	public static Constructor<?> getConstructor(Class<?> clazz, Class<?>... args) throws NoSuchMethodException {
+		Constructor<?> con = clazz.getConstructor(args);
+		con.setAccessible(true);
+		
+		return con;
+	}
+	
+	/**
+	 * Get an Enum from an Enum constant
+	 * @param clazz The Class in which the Enum is defined
+	 * @param constant The name of the Enum Constant
+	 * @return Returns the Enum
+	 * @throws ClassNotFoundException
+	 */
+	public static Enum<?> getEnum(Class<?> clazz, String constant) throws ClassNotFoundException {
+		Class<?> c = Class.forName(clazz.getName());
+		Enum<?>[] constants = (Enum<?>[]) c.getEnumConstants();
+		
+		for(Enum<?> e : constants) {
+			if(e.name().equalsIgnoreCase(constant)) {
+				return e;
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Get an Enum constant by it's name and constant
+	 * @param clazz The Class in which the Enum is defined
+	 * @param enumname The name of the Enum
+	 * @param constant The name of the Constant
+	 * @return Returns the Enum
+	 * @throws ClassNotFoundException
+	 */
+    public static Enum<?> getEnum(Class<?> clazz, String enumname, String constant) throws ClassNotFoundException {
+        Class<?> c = Class.forName(clazz.getName() + "$" + enumname);
+        Enum<?>[] econstants = (Enum<?>[]) c.getEnumConstants();
+        
+        for (Enum<?> e : econstants) {
+            if (e.name().equalsIgnoreCase(constant)) {
+                return e;
+            }
+        }
+
+        return null;
+    }
+    
+    /**
+     * Get a Field
+     * @param clazz The Class in which the Field is defined
+     * @param fieldName The name of the Field
+     * @return Returns the Field
+     * @throws NoSuchFieldException Thrown when the Field was not present in the Class
+     */
+    public static Field getField(Class<?> clazz, String fieldName) throws NoSuchFieldException {
+        Field f = clazz.getDeclaredField(fieldName);
+        f.setAccessible(true);
+        return f;
+    }
+    
+    /**
+     * Get a Method
+     * @param clazz The Class in which the Method is defined
+     * @param methodName The name of the method
+     * @param args The argument types the method takes
+     * @return Returns the Method
+     * @throws NoSuchMethodException
+     */
+    public static Method getMethod(Class<?> clazz, String methodName, Class<?>... args) throws NoSuchMethodException {
+        Method m = clazz.getDeclaredMethod(methodName, args);
+        m.setAccessible(true);
+        return m;
+    }
+    
+    /**
+     * Invoke a Method which takes no arguments. The Class in which the Method is defined is derived from the provided Object
+     * @param obj The object to invoke the method on
+     * @param methodName The name of the Method
+     * @return Returns the result of the method, can be null if the method returns void
+     * @throws NoSuchMethodException
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException
+     */
+    public static Object invokeMethod(Object obj, String methodName) throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    	Method m = getMethod(obj.getClass(), methodName);
+    	return m.invoke(obj);
+    }
+    
+    /**
+     * Invoke a Method where the argument types are derived from the provided arguments. The Class in which the Method is defined is derived from the provided Object
+     * @param obj The object to invoke the method on
+     * @param methodName The name of the Method
+     * @param args The arguments to pass to the Method
+     * @return Returns the result of the method, can be null if the method returns void
+     * @throws NoSuchMethodException
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException
+     */
+    public static Object invokeMethod(Object obj, String methodName, Object[] args) throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    	return invokeMethod(obj.getClass(), obj, methodName, args);
+    }
+    
+    /**
+     * Invoke a Method where the argument types are explicitly given (Helpful when working with primitives). The Class in which the Method is defined is derived from the provided Object.
+     * @param obj The Object to invoke the method on
+     * @param methodName The name of the Method
+     * @param argTypes The types of arguments as a Class array
+     * @param args The arguments as an object array
+     * @return Returns the result of the method, can be null if the method returns void
+     * @throws NoSuchMethodException
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException
+     */
+    public static Object invokeMethod(Object obj, String methodName, Class<?>[] argTypes, Object[] args) throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    	Method m = getMethod(obj.getClass(), methodName, argTypes);
+    	return m.invoke(obj, args);
+    }
+    
+    /**
+     * Invoke a Method where the Class where to find the method is explicitly given (Helpful if the method is located in a superclass). The argument types are derived from the provided arguments
+     * @param clazz The Class where the method is located
+     * @param obj The Object to invoke the method on
+     * @param method The name of the method
+     * @param args The arguments to be passed to the method
+     * @return Returns the result of the method, can be null if the method returns void
+     * @throws NoSuchMethodException
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException
+     */
+    public static Object invokeMethod(Class<?> clazz, Object obj, String method, Object... args) throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    	Class<?>[] argTypes = new Class<?>[args.length];
+    	for(int i = 0; i < args.length; i++) {
+    		argTypes[i] = args[i].getClass();
+    	}
+    	
+    	Method m = getMethod(clazz, method, argTypes);
+    	
+    	return m.invoke(obj, args);
+    }
+    
+    /**
+     * Get the value of a Field, where the Class in which the field is defined is derived from the provided Object
+     * @param obj The object in which the field is located, and from which to get the value
+     * @param name The name of the Field to get the value from
+     * @return Returns the value of the Field
+     * @throws NoSuchFieldException
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     */
+    public static Object getObject(Object obj, String name) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+    	Field f = getField(obj.getClass(), name);
+    	return f.get(obj);
+    }
+    
+    /**
+     * Get the value of a Field, where the Class in which the Field is defined is explicitly given. (Helpful when the Field is in a superclass)
+     * @param obj The Object to get the value from
+     * @param clazz The Class in which the Field is defined
+     * @param name The name of the Field
+     * @return Returns the value of the Field
+     * @throws NoSuchFieldException
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     */
+    public static Object getObject(Object obj, Class<?> clazz, String name) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+    	Field f = getField(clazz, name);
+    	return f.get(obj);
+    }
+    
+    /**
+     * Invoke a Class' constructor. The argument types are derived from the provided arguments
+     * @param clazz The Class in which the Constructor is defined
+     * @param args The arguments to pass to the Constructor
+     * @return Returns an instance of the provided Class in which the Constructor is located
+     * @throws NoSuchMethodException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException
+     */
+    public static Object invokeConstructor(Class<?> clazz, Object... args) throws NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    	Class<?>[] argTypes = new Class<?>[args.length];
+    	for(int i = 0; i < args.length; i++) {
+    		argTypes[i] = args[i].getClass();
+    	}
+    	
+    	Constructor<?> con = getConstructor(clazz, argTypes);
+    	
+    	return con.newInstance(args);
+    }
+    
+    /**
+     * Invoke a Class' Constructor, where the argument types are explicitly given (Helpful when working with primitives)
+     * @param clazz The Class in which the Constructor is defined
+     * @param argTypes The argument types 
+     * @param args The arguments to pass to the constructor
+     * @return Returns an instance of the provided Class in which the Constructor is located
+     * @throws NoSuchMethodException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException
+     */
+    public static Object invokeConstructor(Class<?> clazz, Class<?>[] argTypes, Object[] args) throws NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    	Constructor<?> con = getConstructor(clazz, argTypes);
+    	return con.newInstance(args);
+    }
+}
