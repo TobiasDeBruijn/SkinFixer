e4fe4ac45c6c1f949e522b2396879181f645e00c
Tobias de Bruijn <t.debruijn@array21.dev>
2021-06-11 16:03:57 +0200

    Cleanup

--- src/main/java/nl/thedutchmc/SkinFixer/changeSkin/SkinChangeHandler.java
+++ src/main/java/nl/thedutchmc/SkinFixer/changeSkin/SkinChangeHandler.java
@@ -3,12 +3,8 @@ package nl.thedutchmc.SkinFixer.changeSkin;
 import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
